11880f55df91387c0dc5e450dc0a409452ad17cd
amanpreet5911 <amanpreet.aipl@gmail.com>
2022-11-07 13:02:53 +0530

    register

--- foodonline_main/settings.py
+++ foodonline_main/settings.py
@@ -132,3 +132,10 @@ MEDIA_URL='/media/'
 MEDIA_ROOT=BASE_DIR/'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: '',
+    50: 'danger',
+}
